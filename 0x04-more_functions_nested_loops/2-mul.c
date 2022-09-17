5550dd78853d559ba5dfd60b9e29f9caf1ded0fc
Emelie-alx <okoloanthony8@yahoo.com>
2022-09-17 20:20:22 +0000

    Mutiply 2 inputs

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * mul - Multuply two numbers
+  * @a: First number
+  * @b: Second number
+  * Return: multiply first and second number
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
