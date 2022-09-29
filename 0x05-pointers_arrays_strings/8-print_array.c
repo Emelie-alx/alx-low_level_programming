8fd5c2d1f9be1e399e620a6179073f367020d8e4
Emelie-alx <okoloanthony8@yahoo.com>
2022-09-29 18:32:45 +0000

    Print specified number of int array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * print_array - print a specified number of int array
+  * @a: Input array
+  * @n: Number of array index to be printed
+  */
+void print_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = a; i < n; i++)
+	{
+		j = *(a + i);
+		if (i == (n - 1))
+		{
+			printf("%d", j);
+		}
+		else
+		{
+			printf("%d, ", j);
+		}
+	}
+	printf("\n");
+}
