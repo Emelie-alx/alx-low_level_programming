a360094635ed3b786f519a1e90f39c7577ddec42
Emelie-alx <okoloanthony8@yahoo.com>
2022-09-30 01:16:45 +0000

    Copy a string into another buffer and return \ the string as well

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * _strcpy - Copy an input string to a specified buffer
+  * @dest: Temporary Buffer
+  * @src: string to be copied into dest
+  * Return: pointer to dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i, j, k;
+	char *l;
+
+	dest = src;
+	l = dest;
+	/*for (i = 0; *(dest + i) != 0; i++)
+	{
+		continue;
+	}
+	for (j = 0; j < i; j++)
+	{
+		return (*(l + j));
+	}*/
+	return (l);
+}
