14989ca099969aa795c884747d32d24edfe1bf65
Emelie-alx <okoloanthony8@yahoo.com>
2022-10-05 00:15:10 +0000

    copy a string including the null byte

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,7 +13,7 @@ char *_strcpy(char *dest, char *src)
 	do {
 		*(dest + i) = *(src + i);
 		i++;
-	} while (*(src + i) != 0);
+	} while (*(src + i) != '\0');
 	*(dest + i) = *(src + i);
 	l = dest;
 	return (l);
