5db9c650b75deac1f0f585b59f42b2d2a156c509
Emelie-alx <okoloanthony8@yahoo.com>
2022-10-14 02:45:00 +0000

    Print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,35 +1,25 @@
 #include "main.h"
 /**
-  * print_number - Prints an integer
-  * @n: Input to be printed
+  * print_number - Print an integer
+  * @n: Integer to be printed
   */
 void print_number(int n)
 {
+	int temp, div;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n > 999)
+	div = 1;
+	temp = n;
+	for (; temp > 9; temp /= 10)
 	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n % 1000) / 100) + '0');
-		_putchar(((n % 100) / 10) + '0');
-		_putchar((n % 10) + '0');
+		div *= 10;
 	}
-	else if (n > 99)
+	for (; div >= 1; div /= 10)
 	{
-		_putchar((n / 100) + '0');
-		_putchar(((n % 100) / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n > 9)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else
-	{
-		_putchar(n + '0');
+		_putchar(((n / div) % 10) + '0');
 	}
 }
